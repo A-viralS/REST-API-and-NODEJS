094a0b68a71c469ea433f44b8796e748d418c376
Aviral <soniaviral14@gmail.com>
2024-01-17 10:58:36 +0530

    ssr setup

---
+++ URLShortner/views/url.js
@@ -0,0 +1,11 @@
+// Import any required modules or libraries
+
+// Define any global variables or constants
+
+// Define any helper functions or utility functions
+
+// Define the main function or entry point of the script
+
+// Call the main function or execute the script
+
+// Export any functions or variables that need to be accessed from other files
